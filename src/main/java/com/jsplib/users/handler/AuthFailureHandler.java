8340f870e70667e03054b19c18b2503fd0ee818c
venh <conf312@naver.com>
2023-02-23 15:54:15 +0900

    [add] Spring Security

---
+++ src/main/java/com/jsplib/users/handler/AuthFailureHandler.java
@@ -0,0 +1,45 @@
+package com.jsplib.users.handler;
+
+import com.jsplib.users.member.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final MemberService memberService;
+
+    public AuthFailureHandler(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.info("==> onAuthenticationFailure");
+        String email= request.getParameter("email");
+        String msg = "msg.member.not.match";
+
+        if (exception instanceof DisabledException) {
+            msg = "msg.member.not.enabled";
+        } else if (exception instanceof CredentialsExpiredException) {
+            msg = "msg.member.not.credentialsExpired";
+        } else if (exception instanceof LockedException) {
+            msg = "msg.member.not.accountLocked";
+        }
+
+        //memberService.updateLoginFailCount(email);
+
+        setDefaultFailureUrl("/member/public/login?msg=" + msg + "&email=" + email);
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
---
+++ src/main/java/com/jsplib/users/handler/AuthSuccessHandler.java
@@ -0,0 +1,38 @@
+package com.jsplib.users.handler;
+
+import com.jsplib.users.member.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final MemberService memberService;
+
+    public AuthSuccessHandler(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        log.info("==> onAuthenticationSuccess");
+//        Member.Response member = (Member.Response) authentication.getPrincipal();
+//
+//        if (member.getLoginFailCount() > 4) {
+//            memberService.updateCredentialsNonExpired(member.getId(), "N");
+//            response.sendRedirect("/member/public/login?msg=msg.member.not.credentialsExpired&email=" + member.getEmail());
+//        } else if (member.isEnabled() && member.isCredentialsNonExpired() && member.isAccountNonLocked()) {
+//            memberService.updateLastLoginTime(member.getId(), AuthorizationUtil.getClientIp(request));
+//            RequestContextHolder.getRequestAttributes().setAttribute("member", member, RequestAttributes.SCOPE_SESSION);
+//            response.sendRedirect("/dashboard");
+//        }
+    }
+}
